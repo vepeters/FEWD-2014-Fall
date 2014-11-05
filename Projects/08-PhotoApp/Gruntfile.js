ebccbc0664334cf664dd99b5e8340f2c444a7729
Virginia Peters <vpetersdesign@gmail.com>
2014-11-04 19:57:17 -0500

    adding grunt file

---
+++ Projects/08-PhotoApp/Gruntfile.js
@@ -0,0 +1,50 @@
+/*global module:false*/
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		meta: {
+			banner: '/* \n' +
+					' * <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> \n' +
+					' */ \n'
+		},
+		// CSSMin
+		cssmin: {
+			options: {
+				banner: '<%= meta.banner %>'
+			},
+			combine: {
+				files: '<%= pkg.css %>'
+			}
+		},
+		// Uglify
+		uglify: {
+			target: {
+				keepSpecialComments: 0,
+				banner: '<%= meta.banner %>',
+				files: '<%= pkg.js %>'
+			}
+		},
+		// Watch
+		watch: {
+			css: {
+				files: [ 'css/*', '!css/*.min.css' ],
+				tasks: [ 'cssmin' ]
+			},
+			js: {
+				files: [ 'js/*', '!js/*.min.js' ],
+				tasks: [ 'uglify' ]
+			}
+		}
+	});
+
+	// Load tasks
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Default task.
+	grunt.registerTask('default', [ 'uglify', 'cssmin' ]);
+
+};
